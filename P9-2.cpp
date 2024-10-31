97cd449491c49cbf40142864bebb2bf679522898
braylonrieman14 <b-rieman.2@onu.edu>
2024-10-31 13:23:28 -0400

    Finished P9-2

--- P9-2.cpp
+++ P9-2.cpp
@@ -20,26 +20,44 @@ using namespace std;
 
 int main(void)
 {
-	const int valuesSize = 6;
-	int values[valuesSize] = { 1, 2, 3, 4, 5, 6};
+	while (true)
+	{
+		const int valuesSize = 6;
+		int values[valuesSize];
 
-	//checks if the data is increasing
-	if (isSortedIncreasing(values, valuesSize))
-		cout << "\nThe data is increasing.";
-	else
-		cout << "\nThe data is not increasing.";
+		//input
+		for (int i = 0; i < valuesSize; i++)
+		{
+			cout << "Enter integer #" << i << ", or Q to quit: ";
+			cin >> values[i];
+			if (cin.fail())
+				return 0;
+		}
 
-	//checks if the data is decreasing
-	if (isSortedDecreasing(values, valuesSize))
-		cout << "\nThe data is decreasing.";
-	else
-		cout << "\nThe data is not decreasing.";
+		//checks if the data is increasing
+		if (isSortedIncreasing(values, valuesSize))
+			cout << "\nThe data is increasing.";
+		else
+			cout << "\nThe data is not increasing.";
 
-	//checks if the data has adjacent duplicates
-	if (hasAdjacentDuplicates(values, valuesSize))
-		cout << "\nThe data has adjacent duplicates.";
-	else
-		cout << "\nThe data does not have adjacent duplicates.";
+		//checks if the data is decreasing
+		if (isSortedDecreasing(values, valuesSize))
+			cout << "\nThe data is decreasing.";
+		else
+			cout << "\nThe data is not decreasing.";
+
+		//checks if the data has adjacent duplicates
+		if (hasAdjacentDuplicates(values, valuesSize))
+			cout << "\nThe data has adjacent duplicates.";
+		else
+			cout << "\nThe data does not have adjacent duplicates.";
+
+		//checks if the data has duplicates
+		if (hasDuplicates(values, valuesSize))
+			cout << "\nThe data has duplicates.";
+		else
+			cout << "\nThe data does not have duplicates.\n\n";
+	}
 }
 
 bool isSortedIncreasing(int values[], int size)
@@ -77,13 +95,13 @@ bool hasDuplicates(int values[], int size)
 	for (int i = 0; i < size - 1; i++)
 	{
 		int temp = values[i];
-		for (int i = 0; i < size - 1; i++)
+		for (int j = 0; j < size - 1; j++)
 		{
-			if(temp == values[i])
+			if(temp == values[j] && j != i)
 				return true;
 		}
 	}
 	return false;
-}//hihi
+}
 
 
